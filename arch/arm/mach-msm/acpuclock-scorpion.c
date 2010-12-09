dd8faaa93fb488f607cda3b53c89dea90db1f1ec
Chad Froebel <chad0989@rochester.rr.com>
2010-12-09 09:12:34 -0500

    Boot @ 998mhz

--- arch/arm/mach-msm/acpuclock-scorpion.c
+++ arch/arm/mach-msm/acpuclock-scorpion.c
@@ -506,15 +506,15 @@ static void __init acpuclk_init(void)
 		BUG();
 	}
 
-	/* Move to 768MHz for boot, which is a safe frequency
-	 * for all versions of Scorpion at the moment.
+	/* Move to 998MHz for boot, which is a safe frequency
+	 * for the Incredible.
 	 */
 	speed = acpu_freq_tbl;
 	for (;;) {
-		if (speed->acpu_khz == 768000)
+		if (speed->acpu_khz == 998400)
 			break;
 		if (speed->acpu_khz == 0) {
-			pr_err("acpuclk_init: cannot find 768MHz\n");
+			pr_err("acpuclk_init: cannot find 998MHz\n");
 			BUG();
 		}
 		speed++;
